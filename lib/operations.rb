b1a936eb965f9e5df1d66d52fe4a1ac567f02ab8
github username <github email address>
2019-05-07 14:54:03 +0100

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
-def unsafe?(speed)
+#writing my methods for the lab
 
+#including pry so I can run tests
+require 'pry'
+
+def unsafe?(speed)
+	if speed < 40 || speed > 60
+		true
+	else
+		false
+	end
 end
 
+#puts unsafe?(20) # => expect "true"
+#puts unsafe?(50) # => expect "false"
+#puts unsafe?(100)
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
